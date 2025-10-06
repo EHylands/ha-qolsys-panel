4e97930a7687a58afff8af6082e3a4259e474a0c
Eric Hylands <eric.hylands@gmail.co>
2025-10-05 23:17:21 -0400

    active pairing

--- custom_components/qolsys_panel/__init__.py
+++ custom_components/qolsys_panel/__init__.py
@@ -47,6 +47,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: QolsysPanelConfigEntry) 
     QolsysPanel.select_plugin("remote")
     QolsysPanel.plugin.settings.config_directory = hass.config.config_dir + "/qolsys_panel/"
     QolsysPanel.plugin.settings.plugin_ip = await get_local_ip(hass=hass)
+    QolsysPanel.plugin.settings.mqtt_timeout = 30
+    QolsysPanel.plugin.settings.mqtt_ping = 600
     QolsysPanel.plugin.settings.panel_ip = entry.data[CONF_HOST]
     QolsysPanel.plugin.settings.panel_mac = entry.data[CONF_MAC]
     QolsysPanel.plugin.settings.random_mac = entry.data[CONF_RANDOM_MAC]
@@ -55,6 +57,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: QolsysPanelConfigEntry) 
     QolsysPanel.plugin.check_user_code_on_disarm = False
     QolsysPanel.plugin.log_mqtt_mesages = False
     QolsysPanel.plugin.auto_discover_pki = False
+
 
     # Configure remote plugin
     if not await QolsysPanel.plugin.config(start_pairing=False):
